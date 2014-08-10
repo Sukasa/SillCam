9e0bec27ce37fff4c287196cc9aab126747bac12
Sukasa <nick.m@shaw.ca>
2014-08-10 16:21:23 -0700

    Various updates to improve performance and reliability

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace SillCam
         private static int SavePeriod;
         private static int RollingPtr = 0;
         private static int SaveTimer = 0;
+        private static int FPS = 2;
         private static string OutputFormat;
         private static string Domain;
         private static string DevicePath;
@@ -21,9 +22,9 @@ namespace SillCam
         private static Cameras Camera;
         private static Object LockObj = new Object();
         private static Ivy Ivy;
-
-        private const int FPS = 2;
+        private static ImageConverter Converter = new ImageConverter();
         private static Int64 TimestampStart;
+        private static Picture[] History;
 
         static void Main(string[] args)
         {
@@ -34,8 +35,10 @@ namespace SillCam
             OutputFormat = Properties.Settings.Default.OutputFormat;
             HistorySize = Properties.Settings.Default.HistorySize;
             SavePeriod = Properties.Settings.Default.SavePeriod;
+            FPS = Properties.Settings.Default.FPS;
 
             Snapshots = new Picture[HistorySize];
+            History = new Picture[HistorySize];
 
             // *** Set up Ivy
             Ivy = new Ivy();
@@ -44,14 +47,19 @@ namespace SillCam
             Ivy.BindMsg("SILLCAM_CAPTURE", SaveSinglePicture, null);
             Ivy.DebugProtocol = true;
             Ivy.Start(Domain);
-            
+
             // *** Declare camera device and start streaming in video at 1fps
             Camera = Cameras.DeclareDevice().Named("Camera 1").WithDevicePath(DevicePath).Memorize();
-            Camera.Get("Camera 1").StartVideoStreaming(new PictureSize(640, 480), FPS);
+            Camera.Get("Camera 1").StartVideoStreaming(new PictureSize(480, 360), FPS);
+            DateTime Time = DateTime.Now;
+            DateTime WaitUntil = Time.AddMilliseconds(1000 / FPS);
 
             while (true)
             {
-                Thread.Sleep(1000 / FPS);
+                Time = DateTime.Now;
+                if (Time < WaitUntil)
+                    Thread.Sleep((WaitUntil - Time).Milliseconds);
+                WaitUntil = Time.AddMilliseconds(1000 / FPS);
                 lock (LockObj)
                 {
                     TakePicture();
@@ -67,6 +75,8 @@ namespace SillCam
                         if (SaveTimer == 0)
                         {
                             Ivy.SendMsg("SILLCAM_ROLLINGCOMPLETE:{0}:{1}", TimestampStart, Timestamp());
+                            WriteTime();
+                            Console.WriteLine("Finished rolling save");
                         }
                     }
                     RollingPtr = (++RollingPtr) % HistorySize;
@@ -77,31 +87,66 @@ namespace SillCam
         // *** Save a single picture from the camera buffer
         static void SaveSinglePicture(object sender, IvyMessageEventArgs e)
         {
+            WriteTime();
             Console.WriteLine("Saving single picture");
             lock (LockObj)
             {
-                int Index = (HistorySize - 1 + RollingPtr) % HistorySize;
+                try
+                {
+                    int Index = (HistorySize - 1 + RollingPtr) % HistorySize;
 
-                Int64 i = SavePicture(Index);
-                Ivy.SendMsg("SILLCAM_PICTURE:" + string.Format(OutputFormat, i));
+                    Int64 i = SavePicture(Index);
+                    Ivy.SendMsg("SILLCAM_PICTURE:" + string.Format(OutputFormat, i / 1000, i % 1000));
+                }
+                catch (Exception ex)
+                {
+                    WriteTime();
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                    Console.WriteLine((HistorySize - 1 + RollingPtr) % HistorySize);
+                }
             }
+        }
+
+        static void SaveHistoryWorker(object History)
+        {
+            Picture[] Pictures = (Picture[])History;
+            foreach (Picture Snapshot in Pictures)
+            {
+                if (Snapshot.PictureData == null)
+                    continue;
+                Int64 _Timestamp = Timestamp(Snapshot.Timestamp);
+                String Filename = string.Format(OutputFormat, _Timestamp / 1000, _Timestamp % 1000);
+                Image img = (Image)Converter.ConvertFrom(Snapshot.PictureData);
+                Thread.Sleep(100);
+                img.Save(Filename);
+                Thread.Sleep(100);
+            }
+            WriteTime();
+            Console.WriteLine("History Saved.");
         }
 
         // *** Save the entire camera buffer, and schedule continuing saves as we take more pictures
         static void SaveHistory(object sender, IvyMessageEventArgs e)
         {
-            Console.WriteLine("Saving history");
-            int Count = 0;
             lock (LockObj)
             {
                 if (SaveTimer == 0)
                 {
-                    TimestampStart = Timestamp();
+                    WriteTime();
+                    Console.WriteLine("Saving history");
 
-                    for (int SaveIndex = RollingPtr; Count < HistorySize; SaveIndex = ++SaveIndex % HistorySize, Count++)
-                    {
-                        SavePicture(SaveIndex);
-                    }
+                    Array.Copy(Snapshots, History, HistorySize);
+
+                    Thread Worker = new Thread(Program.SaveHistoryWorker);
+                    Worker.Priority = ThreadPriority.Lowest;
+                    Worker.IsBackground = true;
+                    Worker.Start(History);
+                }
+                else
+                {
+                    WriteTime();
+                    Console.WriteLine("Extending rolling save period");
                 }
                 SaveTimer = SavePeriod;
             }
@@ -113,35 +158,31 @@ namespace SillCam
             Picture Snapshot = Snapshots[Index];
             if (Snapshot.PictureData == null)
             {
+                WriteTime();
                 Console.WriteLine("Failed to save snapshot: no data");
                 return Timestamp();
             }
 
             if (Snapshot.PictureData.Length < 5)
             {
+                WriteTime();
                 Console.WriteLine("Failed to save snapshot: truncated data");
                 return Timestamp();
             }
 
-            if (Snapshot.Timestamp.Year < 2000)
-            {
-                Console.WriteLine("Failed to save snapshot: invalid timestamp");
-                return Timestamp();
-            }
-
-            ImageConverter ic = new ImageConverter();
             try
             {
-                Image img = (Image)ic.ConvertFrom(Snapshot.PictureData);
-
-                String Filename = string.Format(OutputFormat, Timestamp(Snapshot.Timestamp));
+                Image img = (Image)Converter.ConvertFrom(Snapshot.PictureData);
+                Int64 _Timestamp = Timestamp(Snapshot.Timestamp);
+                String Filename = string.Format(OutputFormat, _Timestamp / 1000, _Timestamp % 1000);
                 img.Save(Filename);
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine("ERROR");
-                return Timestamp();
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
             return Timestamp();
         }
@@ -149,16 +190,19 @@ namespace SillCam
         // *** Pull in a picture from the camera stream
         static void TakePicture()
         {
-            Picture Snapshot;
-            Snapshot.PictureData = Camera.Get("Camera 1").GetVideoFrame();
-            Snapshot.Timestamp = DateTime.Now;
-            Snapshots[RollingPtr] = Snapshot;
+            Snapshots[RollingPtr].PictureData = Camera.Get("Camera 1").GetVideoFrame();
+            Snapshots[RollingPtr].Timestamp = DateTime.Now;
         }
 
         private struct Picture
         {
             public byte[] PictureData;
             public DateTime Timestamp;
+        }
+
+        public static void WriteTime()
+        {
+            Console.Write(DateTime.Now.ToString("HH:mm:ss "));
         }
 
         public static Int64 Timestamp()
@@ -168,7 +212,7 @@ namespace SillCam
 
         public static Int64 Timestamp(DateTime dateTime)
         {
-            return (Int64)(dateTime - new DateTime(1970, 1, 1).ToLocalTime()).TotalSeconds;
+            return (Int64)(dateTime - new DateTime(1970, 1, 1).ToLocalTime()).TotalMilliseconds;
         }
     }
 }
